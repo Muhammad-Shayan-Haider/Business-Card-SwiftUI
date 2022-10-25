ede445f78e4599515d47ffca7d87409ed710afee
Muhammad Shayan <shayanhaider1999@gmail.com>
2022-10-25 15:16:58 +0500

    Initial Commit

---
+++ BusinessCardApp/BusinessCardAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  BusinessCardAppApp.swift
+//  BusinessCardApp
+//
+//  Created by Muhammad Shayyan on 25/10/2022.
+//
+
+import SwiftUI
+
+@main
+struct BusinessCardAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
